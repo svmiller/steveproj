3a5e2bbf8cf406ab161f9067ce175d6e8d22ec7b
svmiller <steven.v.miller@gmail.com>
2021-03-20 13:08:12 -0400

    move to {bookdown}. HT @andrewheiss

--- inst/skeleton/src/render_docx.R
+++ inst/skeleton/src/render_docx.R
@@ -1,1 +1,8 @@
-rmarkdown::render("ms.Rmd", output_file="doc/ms.docx", rmarkdown::word_document(reference_docx = stevetemplates::templ_word()))
+# rmarkdown::render("ms.Rmd", output_file="doc/ms.docx", rmarkdown::word_document(reference_docx = stevetemplates::templ_word()))
+# ^ old, legacy, here if you want to revert to it.
+
+rmarkdown::render("ms.Rmd", output_file="doc/ms.docx",
+                  bookdown::word_document2(reference_docx = stevetemplates::templ_word(),
+                                           toc = FALSE, number_sections = FALSE))
+
+# ^ {bookdown} has table/figure cross-ref ability.
